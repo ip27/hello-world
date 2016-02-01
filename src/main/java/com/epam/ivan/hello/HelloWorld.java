657f930eb4121aac140043854a75887b27bbd21c
Ivan Petrov <ip27@bk.ru>
2016-02-01 17:45:09 +0600

    Fix HelloWorld output

--- src/main/java/com/epam/ivan/hello/HelloWorld.java
+++ src/main/java/com/epam/ivan/hello/HelloWorld.java
@@ -2,6 +2,6 @@ package com.epam.ivan.hello;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("lskdrhgjksdhflghk");
+        System.out.println("Hello World");
     }
 }
